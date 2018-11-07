63707ec5b1be8463c6982476e3ed79c36678f720
mike seibel <mikes@washington.edu>
2018-11-07 14:54:45 -0800

    urls

--- scheduled_job_client/urls.py
+++ scheduled_job_client/urls.py
@@ -3,5 +3,5 @@ from scheduled_job_client.views.notification import JobClient
 
 
 urlpatterns = [
-    url(r'job/notification$', JobClient.as_view(), name='notification'),
+    url(r'notification$', JobClient.as_view(), name='notification'),
 ]
